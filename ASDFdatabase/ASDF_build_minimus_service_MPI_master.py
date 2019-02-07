82ebf24d31e1699ac8097a25869fde7b21b2aac8
ashbycooper <ashby.cooper@odysseygeo.com.au>
2019-02-08 10:42:28 +1100

    Initial commit of script to convert miniseed data from Minimus units (arranged in GA's AusArray directory structure), into individual ASDF files for each station/service interval.

--- ASDFdatabase/ASDF_build_minimus_service_MPI_master.py
+++ ASDFdatabase/ASDF_build_minimus_service_MPI_master.py
@@ -127,48 +127,131 @@
 #
 # print(exec_str)
 
+# from mpi4py import MPI
+# import sys, time
+#
+# ################ Set up MPI variables ################
+#
+# comm = MPI.COMM_WORLD
+# rank = comm.Get_rank()
+# size = comm.Get_size()
+# name = MPI.Get_processor_name()
+# status = MPI.Status()
+#
+# ################ Master code ################
+#
+# print("MPI settings")
+#
+# print(rank)
+# print(size)
+# print(name)
+# print(status)
+#
+# print("----------")
+#
+# icomms_dict = {}
+# script = 'ASDF_build_minimus_service_MPI_spawn.py'
+#
+# if rank == 0:
+#     for _i in range(size-1):
+#         child_rank = _i + 1
+#         try:
+#            print('sending info to child process {}...'.format(child_rank))
+#            icomm = MPI.COMM_SELF.Spawn(sys.executable, args=[script], maxprocs=1, root=0)
+#            icomm.send("I am a child_rank {}".format(child_rank), dest=0, tag=11)
+#            icomms_dict[child_rank] = {"icomm": icomm, "status": True}
+#            print('Spawned a child.')
+#         except: ValueError('Spawn failed to start.')
+#
+# completed_dict = {}
+# work_finished = False
+#
+# while not work_finished:
+#     for mpi_rank, icomm_info in icomms_dict.items():
+#         icomm = icomm_info["icomm"]
+#
+#         if icomm.Iprobe(source=0, tag=MPI.ANY_TAG):
+#             print('{} child responded...'.format(mpi_rank))
+#             rec_msg = icomm.recv(source=0, tag=MPI.ANY_TAG)
+#             print(icomm.Get_rank())
+#             icomm.Disconnect()
+#             icomms_dict[mpi_rank]["status"] = False
+#
+#         else:
+#             print('{} child doing some work...'.format(mpi_rank))
+#
+#     if all(value["status"] == False for value in icomms_dict.values()):
+#         work_finished = True
+#
+#     else:
+#         # sleep and wait another period of time to recheck for msg
+#         time.sleep(10)
+#
+#
+# print('work complete')
+
 from mpi4py import MPI
-import datetime, sys, numpy, time
 
-################ Set up MPI variables ################
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
-size = comm.Get_size()
-name = MPI.Get_processor_name()
-status = MPI.Status()
+def enum(*sequential, **named):
+    """Handy way to fake an enumerated type in Python
+    http://stackoverflow.com/questions/36932/how-can-i-represent-an-enum-in-python
+    """
+    enums = dict(zip(sequential, range(len(sequential))), **named)
+    return type('Enum', (), enums)
 
-################ Master code ################
 
-icomms = []
-script = 'ASDF_build_minimus_service_MPI_child.py'
-for child_rank in range(size):
-    if child_rank == 0:
-        pass
-    else:
-        try:
-           print('Trying to spawn child process {}...'.format(child_rank))
-           icomm = MPI.COMM_SELF.Spawn(sys.executable, args=[script], maxprocs=1, root=0)
-           icomm.send("I am a child_rank {}".format(child_rank), dest=0, tag=11)
-           icomms.append(icomm)
-           print('Spawned a child.')
-        except: ValueError('Spawn failed to start.')
+# Define MPI message tags
+tags = enum('READY', 'DONE', 'EXIT', 'START')
 
-completed = False
-while not completed and icomms:
-    for icomm in icomms:
-        if icomm.Iprobe(source=0, tag=MPI.ANY_TAG):
-            print('A child responded...')
-            solved\
-                = icomm.recv(source=0, tag=MPI.ANY_TAG)
-            icomm.Disconnect()
-            icomms.remove(icomm)
-            if solved: break
-    if not solved:
-        print('spawns doing some work...')
-        time.sleep(1)
-# make sure all pending sends get matched
-for icomm in icomms:
-    icomm.recv(source=0, tag=MPI.ANY_TAG)
-    icomm.Disconnect()
-print('received solution: %d' % solved)
+# Initializations and preliminaries
+comm = MPI.COMM_WORLD  # get MPI communicator object
+size = comm.size  # total number of processes
+rank = comm.rank  # rank of this process
+status = MPI.Status()  # get MPI status object
+
+if rank == 0:
+    # Master process executes code below
+    tasks = range(10 * size)
+    task_index = 0
+    num_workers = size - 1
+    closed_workers = 0
+    print("Master starting with %d workers" % num_workers)
+    while closed_workers < num_workers:
+        data = comm.recv(source=MPI.ANY_SOURCE, tag=MPI.ANY_TAG, status=status)
+        source = status.Get_source()
+        tag = status.Get_tag()
+        if tag == tags.READY:
+            # Worker is ready, so send it a task
+            if task_index < len(tasks):
+                comm.send(tasks[task_index], dest=source, tag=tags.START)
+                print("Sending task %d to worker %d" % (task_index, source))
+                task_index += 1
+            else:
+                comm.send(None, dest=source, tag=tags.EXIT)
+        elif tag == tags.DONE:
+            results = data
+            print("Got data from worker %d" % source)
+        elif tag == tags.EXIT:
+            print("Worker %d exited." % source)
+            closed_workers += 1
+
+    print("Master finishing")
+
+else:
+    # Worker processes execute code below
+    name = MPI.Get_processor_name()
+    print("I am a worker with rank %d on %s." % (rank, name))
+    while True:
+        comm.send(None, dest=0, tag=tags.READY)
+        task = comm.recv(source=0, tag=MPI.ANY_TAG, status=status)
+        tag = status.Get_tag()
+
+        if tag == tags.START:
+            # Do the work here
+            result = task ** 2
+            comm.send(result, dest=0, tag=tags.DONE)
+        elif tag == tags.EXIT:
+            break
+
+    comm.send(None, dest=0, tag=tags.EXIT)
